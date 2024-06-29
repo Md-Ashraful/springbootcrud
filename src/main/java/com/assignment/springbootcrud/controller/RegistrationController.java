8c40ceab732be744f310b564a280369bb4c4b09e
md-ashraful <ashraf314@outlook.com>
2024-06-29 14:09:46 +0600

    added api's for update, delete, read user info

--- src/main/java/com/assignment/springbootcrud/controller/RegistrationController.java
+++ src/main/java/com/assignment/springbootcrud/controller/RegistrationController.java
@@ -15,12 +15,6 @@ public class RegistrationController {
     @Autowired
     private RegistrationService registrationService;
 
-    @GetMapping
-    public ResponseEntity<?> getUsers(){
-        var users = registrationService.getAllUsers();
-        return new ResponseEntity<>(users,HttpStatus.OK
-        );
-    }
 
     @PostMapping("/registration")
     public ResponseEntity<?> registerUser( @RequestBody User userInfo){
@@ -29,5 +23,30 @@ public class RegistrationController {
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
 
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getUsers(){
+        var users = registrationService.getAllUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable("id") Long id){
+        var user = registrationService.getUserById(id);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") Long id, @RequestBody User userInfo){
+        var user = registrationService.updateUser(id,userInfo);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id){
+        registrationService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
